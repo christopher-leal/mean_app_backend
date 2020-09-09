1c7c4a55d31221f1510d0a82b5177e273244350a
christopher-leal <chris_t_o@hotmail.com>
2020-09-09 14:12:34 -0500

    breakline

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use('/api/uploads', require('./src/routes/uploads'));
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
+
 app.listen(process.env.PORT, () => {
 	console.log('Servidor corriendo en el puerto: ' + process.env.PORT);
 });
